f78849b1bd232b99fcd0b19df8937f6e1b6331d5
smallChenn <chenbingjie1998@gmail.com>
2023-02-18 23:15:45 -0600

    rough commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import React from 'react';
-import 'materialize-css/dist/css/materialize.min.css';
-import 'materialize-css/dist/js/materialize.min.js';
-import { Route, Routes } from 'react-router-dom';
-import Home from './components/pages/Home';
-import Skills from './components/pages/skills/Skills';
-import Experiences from './components/pages/experiences/Experiences';
-import Educations from './components/pages/educations/Educations';
-import Portfolios from './components/pages/portfolios/Portfolios';
+import React, { Component } from 'react';
 import './App.css';
-import { Divider } from '@material-ui/core';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Sidebar from './components/sidebar/Sidebar';
+import About from './components/about/About';
+import Education from './components/Education/Educations';
+import Interest from './components/skills/Interest'
+import CarouselImages from './components/carousel/CarouselImages.js'
 
-function App() {
+
+class App extends Component {
+
+  render() {
     return (
-      <Routes>
-     
-        <Route exact path="/" element={ <Home/> } />
-         
-        <Route path="/skills" element={Skills} />
-        <Route path="/experiences" element={Experiences} />
-        <Route path="/educations" element={Educations} />
-        <Route path="/portfolios" element={Portfolios} />
-    </Routes>
-   
+      <Router>
+        <div className="App">
+          <div className="side">
+            <nav className="navbar side navbar-expand-lg navbar-light p-0" >
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" style={{ zIndex: '1' }}>
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <Sidebar />
+              </div>
+            </nav>
+          </div>
+          <div className="main">
+            <div>
+
+              <CarouselImages />
+            </div>
+            <About />
+            <Interest />
+            <Education />
+          </div>
+        </div>
+      </Router>
     );
   }
-  
-  export default App;
+}
+
+export default App;
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './Sidebar.css';
+import { HashLink as Link } from 'react-router-hash-link';
+import logo from '../images/hello.jpg'
+
+class Sidebar extends Component {
+    render() {
+
+        return (
+            <div className="sidebar">
+                <div style={{ color: 'black', fontWeight: 'bold' }} className="tagtop"> # Woman in Tech </div>
+                <div style={{ color: 'black', fontWeight: 'bold' }} className=""> # hello_world </div>
+                <div style={{ color: 'black', fontWeight: 'bold' }} className=""> # coding </div>
+                <h1><Link smooth to="/#start" className="h1_links">Bingjie Chen</Link></h1>
+
+                <img src={logo} />
+                
+                <ul className="sidebar-nav">
+                    <li className="sidebar-nav-items"><Link smooth to="/#about" className="links">About</Link></li>
+                    <li className="sidebar-nav-items"><Link smooth to="/#education" className="links" >Education</Link></li>
+                    <li className="sidebar-nav-items"><Link smooth to="/#interest" className="links">Interest</Link></li>
+                  
+                </ul>
+
+                <div className="flip-card-back">
+                    <ul className="sidebar-nav">
+                        <li className="sidebar-nav-icons"> <a href="https://github.com/smallChenn" rel="opener noreferrer" target="_blank" className="fa fas fa-github fa-lg"></a></li>
+                        <li className="sidebar-nav-icons"><a href="https://www.linkedin.com/in/chenbingjie1998/" rel="opener noreferrer" target="_blank" className="fa fas fa-linkedin fa-lg"></a></li>
+                        <li className="sidebar-nav-icons"> <a href="chenbingjie1998@gmail.com" rel="opener noreferrer" target="_blank" className="fa fas fa-envelope fa-lg"></a></li>
+                        </ul>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default Sidebar
