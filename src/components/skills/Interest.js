7c6cc712fa746ea126b7fdeeb45065b4e4724090
smallChenn <chenbingjie1998@gmail.com>
2023-02-20 19:19:27 -0600

    commit

--- src/components/skills/Interest.js
+++ src/components/skills/Interest.js
@@ -2,31 +2,40 @@ import React, { Component } from 'react'
 import classes from './Interest.module.css';
 import ScrollAnimation from 'react-animate-on-scroll';
 import "animate.css/animate.min.css";
+import Pain from '../images/painting.png'
+import Travel from '../images/travel.png'
 
 class Interest extends Component {
   render() {
     return (
       <div className={classes.box} id="interest">
         <ScrollAnimation offset={0} animateIn="fadeInLeft" duration={2.4} animateOnce={true} initiallyVisible={true}>
-          <span className={classes.head}>WHAT I DO?</span>
-          <h2 className={classes.heading}>HERE ARE SOME OF MY EXPERTISE</h2>
+          <span className={classes.head}>What I like😍</span>
+          <h2 className={classes.heading}>HERE ARE SOME OF MY Interests</h2>
           <div className={classes.Interest}>
             <ScrollAnimation offset={0} animateIn="fadeInLeft" duration={2.4} animateOnce={true} initiallyVisible={true}>
               <div className={classes.web}>
-                <h3>APP Develpoment</h3>
-                <p>I  </p>
+                <h3>Drawing </h3>
+                <img src={Pain} />
+
               </div>
             </ScrollAnimation>
             <ScrollAnimation offset={0} animateIn="fadeInLeft" duration={2.4} animateOnce={true} initiallyVisible={true}>
               <div className={classes.app}>
-                <h3>Web Develpoment</h3>
-                <p>I am a full stack web developer(MERN) and build websites using HTML, CSS, Javascript, ReactJs.<br /> I have also good grasp on Node and MongoDb</p>
+                <h3>Traveling</h3>
+                <img src={Travel} />
               </div>
             </ScrollAnimation>
             <ScrollAnimation offset={0} animateIn="fadeInLeft" duration={2.4} animateOnce={true} initiallyVisible={true}>
               <div className={classes.other}>
-                <h3>Other's Interest</h3>
-                <p>I also have decent hand in developing website with WordPress.<br />Github </p>
+                <h3>Surfing</h3>
+                <p>Still expolaring... </p>
+                <p>Hope can share my surfing photo soonly!</p>
+                <p>🏄</p>
+                <p>🏄</p>
+                <p>🏄</p>
+                <p>🏄</p>
+                
               </div>
             </ScrollAnimation>
           </div>
